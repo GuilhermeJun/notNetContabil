50e57ab8b5802a80ba5a9560abbe70aef5eb595a
GuilhermeJun <guilherme.jun@hotmail.com>
2026-04-12 17:13:52 -0300

    adjust veriables en RegistroContabilDTO

--- src/SistemaContabil.Application/DTOs/RegistroContabilDto.cs
+++ src/SistemaContabil.Application/DTOs/RegistroContabilDto.cs
@@ -23,13 +23,13 @@ public class RegistroContabilDto
     /// Identificador da conta associada
     /// </summary>
     [Required(ErrorMessage = "ID da conta é obrigatório")]
-    public int ContaIdConta { get; set; }
+    public int ContaId { get; set; }
 
     /// <summary>
     /// Identificador do centro de custo associado
     /// </summary>
     [Required(ErrorMessage = "ID do centro de custo é obrigatório")]
-    public int CentroCustoIdCentroCusto { get; set; }
+    public int CentroCustoId { get; set; }
 
     /// <summary>
     /// Nome da conta associada
